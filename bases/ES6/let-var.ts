2ba8b758c25ba00a168e566540192e3c20253317
Carlos Calzadilla <maleras24@gmail.com>
2025-03-03 08:13:59 +0000

    Seccion 7 terminada

--- bases/ES6/let-var.ts
+++ bases/ES6/let-var.ts
@@ -1,3 +1,13 @@
 (() => {
-    console.log("let")
+    
+    // console.log(a);
+    
+    const nombre: string = 'Fernando';
+
+    const getNombre = (): void => {
+        console.log('Viejo gerName');
+    }
+
+    // gerName = () => { console.log('Nuego getName')};
+    // getName();
 })()
